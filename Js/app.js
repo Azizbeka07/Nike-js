02572e6bfc48ad082c82986fa47c110941027712
AKTIV <Azizbek>
2023-04-06 16:48:46 +0500

    hello

--- Js/app.js
+++ Js/app.js
@@ -5,19 +5,21 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const closeBtn = document.querySelector(".mobile-menu__close-btn");
 const modalWindow = document.querySelector(".modal-window");
 const shpoNow = document.querySelector(".header-content__btn");
-const modalClose = document.querySelector(".modal-window__btn")
+const modalClose = document.querySelector(".modal-window__btn");
+const body = document.querySelector("body");
 menu.addEventListener("click", () => {
   mobileMenu.classList.add("open");
 });
 
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("open");
-})
+ document.body.style.overflow="hidden"
+});
 
 shpoNow.addEventListener("click", () => {
   modalWindow.classList.add("open");
 });
 
-modalClose.addEventListener('click', () => {
-    modalWindow.classList.remove("open")
-})
+modalClose.addEventListener("click", () => {
+  modalWindow.classList.remove("open");
+});
