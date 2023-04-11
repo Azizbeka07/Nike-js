caf1d4e19286d2670b566e69bfbe21ab1181d58c
hgvh <you@example.com>
2023-04-11 17:26:52 +0500

    jjjjjjjj

--- Js/app.js
+++ Js/app.js
@@ -1,21 +1,22 @@
-"use strict";
-
+// "use strict";
+// burger menu and modal window
 const menu = document.querySelector(".burger-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const closeBtn = document.querySelector(".mobile-menu__close-btn");
 const modalWindow = document.querySelector(".modal-window");
-const shpoNow = document.querySelector(".header-content__btn");
-const modalClose = document.querySelector(".modal-window__btn");
+const shpoNow = document.querySelector(".button-2");
+const modalClose = document.querySelector(".modal-window__close-btn");
 const body = document.querySelector("body");
+
+
 menu.addEventListener("click", () => {
   mobileMenu.classList.add("open");
 });
 
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("open");
- document.body.style.overflow="hidden"
 });
-
+// modal window open
 shpoNow.addEventListener("click", () => {
   modalWindow.classList.add("open");
 });
