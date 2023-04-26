74bb7f732ce3f3a101bc600476758f33d7e2a1ed
hgvh <you@example.com>
2023-04-26 14:30:42 +0500

    animation

--- Js/app.js
+++ Js/app.js
@@ -8,7 +8,6 @@ const shpoNow = document.querySelector(".button-2");
 const modalClose = document.querySelector(".modal-window__close-btn");
 const body = document.querySelector("body");
 
-
 menu.addEventListener("click", () => {
   mobileMenu.classList.add("open");
 });
@@ -24,7 +23,7 @@ shpoNow.addEventListener("click", () => {
 
 modalClose.addEventListener("click", () => {
   modalWindow.classList.remove("open");
-  document.body.style.overflow = "scroll"
+  document.body.style.overflow = "scroll";
 });
 
 // header banner right carousel
@@ -41,3 +40,67 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+
+// NUMBER ANIMATION
+
+const item = document.querySelector(".item");
+const timer = document.querySelectorAll(".timer");
+
+window.addEventListener("scroll", function scroll() {
+  if (this.scrollY > item.offsetTop - item.clientHeight * 3) {
+    for (let i = 0; i < timer.length; i++) {
+      let count = timer[i].getAttribute("count");
+      function rec(j = 0) {
+        timer[i].innerHTML = j;
+        j++;
+        if (j <= count) {
+          setTimeout(() => {
+            rec(j);
+          }, 50);
+        }
+      }
+      rec();
+      window.removeEventListener("scroll", scroll)
+    }
+  }
+});
+
+// HEADER TOP ANIMATION
+
+window.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+  const headerItem = document.querySelectorAll(".header-item");
+
+      headerItem.forEach((item) => {
+        item.style = `transform: translateY(0)`
+      })
+  
+})
+
+// HEADER BANNER ANIMATION
+
+window.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelector(".wrapper");
+  const wrapperItem = document.querySelectorAll(".wrapper-item");
+
+      wrapperItem.forEach((item) => {
+        item.style = `transform: translateX(100px)`
+      })
+  
+})
+
+// CARD SECTION ANIMATION
+
+window.addEventListener("DOMContentLoaded", () => {
+  const content = document.querySelector(".content");
+  const contentItem = document.querySelectorAll(".content-item");
+
+  window.addEventListener("scroll", () => {
+    if(window.scrollY > content.offsetTop - content.clientHeight) {
+      contentItem.forEach((item) => {
+        item.style = `transform: translateX(0)`
+        item.style = `transform: translateY(0)`
+      })
+    }
+  })
+})
